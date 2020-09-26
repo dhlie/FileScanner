b2d4526f85c15f9deee49ba0ed4e2691c31cc7b8
Haoliang <duanhaoliang@dayuwuxian.com>
2020-09-26 13:58:05 +0800

    change ui

--- app/src/main/java/d/hl/filescan/MainActivity.java
+++ app/src/main/java/d/hl/filescan/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -20,19 +21,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import static android.util.TypedValue.COMPLEX_UNIT_DIP;
+
 public class MainActivity extends Activity implements View.OnClickListener {
 
   private TextView mTVINfo;
   private EditText mETThread, mETDepth;
-  private CheckBox mCBDetail;
+  private CheckBox mCBDetail, mCBHideDir, mCBNoMedia;
 
   private MyAdapter mAdapter = new MyAdapter();
   private FileScanner mFileScanner;
-  private String[] mSuffixes;
   private long mStartTime;
-  private int mThreadCount, mScanDepth;
-  private boolean mNeedDetail;
-  private String mScanPath[];
 
   private FileScanner.ScanCallback callback = new AbstractScanCallback() {
     @Override
@@ -41,7 +40,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
       runOnUiThread(new Runnable() {
         @Override
         public void run() {
-          mTVINfo.setText("扫描中...  total:0 time:0 thd:" + mThreadCount + "  dp:" + mScanDepth);
+          String thd = mETThread.getText().toString();
+          int threadCount = Integer.parseInt(TextUtils.isEmpty(thd) ? "1" : thd);
+          String deep = mETDepth.getText().toString();
+          int scanDepth = Integer.parseInt(TextUtils.isEmpty(deep) ? "-1" : deep);
+
+          mTVINfo.setText("扫描中...  total:0 time:0 线程:" + threadCount + "  层级:" + scanDepth);
           mAdapter.changeData(null);
         }
       });
@@ -56,7 +60,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
       runOnUiThread(new Runnable() {
         @Override
         public void run() {
-          mTVINfo.setText("total:" + count + " time:" + time + " thd:" + mThreadCount + "  dp:" + mScanDepth);
+          String thd = mETThread.getText().toString();
+          int threadCount = Integer.parseInt(TextUtils.isEmpty(thd) ? "1" : thd);
+          String deep = mETDepth.getText().toString();
+          int scanDepth = Integer.parseInt(TextUtils.isEmpty(deep) ? "-1" : deep);
+
+          mTVINfo.setText("total:" + count + " time:" + time + " 线程:" + threadCount + "  层级:" + scanDepth);
           mAdapter.changeData(files);
           mFileScanner.recycle();
           mFileScanner = null;
@@ -74,32 +83,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
     Button btnStop = (Button) findViewById(R.id.tv_stop);
     mTVINfo = (TextView) findViewById(R.id.tv_info);
     ListView lvList = (ListView) findViewById(R.id.lv);
-    Button btnSet = (Button) findViewById(R.id.tv_reset);
     mETThread = (EditText) findViewById(R.id.et_thd);
     mETDepth = (EditText) findViewById(R.id.et_depth);
     mCBDetail = (CheckBox) findViewById(R.id.cb_detail);
+    mCBHideDir = (CheckBox) findViewById(R.id.cb_hiddir);
+    mCBNoMedia = (CheckBox) findViewById(R.id.cb_nomedia);
 
     btnStart.setOnClickListener(this);
     btnStop.setOnClickListener(this);
-    btnSet.setOnClickListener(this);
     lvList.setAdapter(mAdapter);
 
-    int processorNum = Runtime.getRuntime().availableProcessors();
-    mThreadCount = Math.min(processorNum, 4);
-    mScanDepth = -1;//全盘扫描
-    mNeedDetail = false;//是否获取文件详情
-    mSuffixes = new String[]{};//查找所有文件
-    //mSuffixes = new String[]{"mp3", "mp4", "avi", "rmvb"};
-    //mSuffixes = new String[]{"jpg", "jpeg", "png", "bmp", "gif"};
-    mSuffixes = new String[]{"jpg", "jpeg", "png", "bmp", "gif", "mp3", "mp4", "avi", "rmvb", "wmv", "wma", "flav", "wav", "ogg", "mp2", "m4a", "au", "aac", "3gp", "3g2", "asf", "flv", "mov", "rm", "swf", "mpg", "EBK2", "EBK3", "TXT", "EPUB", "CHM", "UMD", "PDF", "OPUB", "DOC", "DOCX",
-            "WPS", "XLS", "XLSX", "ET", "PPT", "PPTX", "DPS"};
-    String sdPath = Environment.getExternalStorageDirectory().getAbsolutePath();
-    mScanPath = new String[]{sdPath + "/"};
-    //mScanPath = new String[] {sdPath+"/Android/", sdPath+"/DCIM/"};
-
-    mETDepth.setText("" + mScanDepth);
-    mETThread.setText("" + mThreadCount);
-    mCBDetail.setChecked(mNeedDetail);
+    mETDepth.setText("-1");
+    mETThread.setText("4");
+    mCBDetail.setChecked(true);
+    mCBHideDir.setChecked(true);
+    mCBNoMedia.setChecked(true);
 
     String[] requiredPers = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -114,9 +112,22 @@ public class MainActivity extends Activity implements View.OnClickListener {
     if (mFileScanner == null) {
       mFileScanner = new FileScanner();
     }
-    mFileScanner.setHideDirScanEnable(false);
-    mFileScanner.setNoMediaDirScanEnable(false);
-    mFileScanner.initScanner(mSuffixes, mThreadCount, mScanDepth, mNeedDetail);
+
+    String[] suffixes = new String[]{};//查找所有文件
+    //suffixes = new String[]{"mp3", "mp4", "avi", "rmvb"};
+    //suffixes = new String[]{"jpg", "jpeg", "png", "bmp", "gif"};
+    suffixes = new String[]{"jpg", "jpeg", "png", "bmp", "gif", "mp3", "mp4", "avi", "rmvb", "wmv", "wma", "flav", "wav", "ogg", "mp2", "m4a", "au", "aac", "3gp", "3g2", "asf", "flv", "mov", "rm", "swf", "mpg", "EBK2", "EBK3", "TXT", "EPUB", "CHM", "UMD", "PDF", "OPUB", "DOC", "DOCX",
+            "WPS", "XLS", "XLSX", "ET", "PPT", "PPTX", "DPS"};
+
+    String thd = mETThread.getText().toString();
+    int threadCount = Integer.parseInt(TextUtils.isEmpty(thd) ? "1" : thd);
+
+    String deep = mETDepth.getText().toString();
+    int scanDepth = Integer.parseInt(TextUtils.isEmpty(deep) ? "-1" : deep);
+
+    mFileScanner.setHideDirScanEnable(mCBHideDir.isChecked());
+    mFileScanner.setNoMediaDirScanEnable(mCBNoMedia.isChecked());
+    mFileScanner.initScanner(suffixes, threadCount, scanDepth, mCBDetail.isChecked());
     mFileScanner.setScanCallback(callback);
   }
 
@@ -125,20 +136,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
     switch (view.getId()) {
       case R.id.tv_start:
         initFileScanner();
-        mFileScanner.startScan(mScanPath);
+
+        String sdPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String[] scanPath = new String[]{sdPath};
+        //scanPath = new String[] {sdPath+"/Android/", sdPath+"/DCIM/"};
+        mFileScanner.startScan(scanPath);
         break;
       case R.id.tv_stop:
         if (mFileScanner != null) mFileScanner.stopScan();
-        break;
-      case R.id.tv_reset:
-        String thd = mETThread.getText().toString();
-        mThreadCount = Integer.parseInt(TextUtils.isEmpty(thd) ? "1" : thd);
-        String deep = mETDepth.getText().toString();
-        mScanDepth = Integer.parseInt(TextUtils.isEmpty(deep) ? "-1" : deep);
-        mNeedDetail = mCBDetail.isChecked();
-        initFileScanner();
-        mETDepth.setText("" + mScanDepth);
-        mETThread.setText("" + mThreadCount);
         break;
     }
   }
@@ -146,7 +151,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
   private class MyAdapter extends BaseAdapter {
 
     private List<FileScanner.FindItem> mData;
-    private SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd");
     private Date mDate = new Date();
 
     public void changeData(List<FileScanner.FindItem> items) {
@@ -164,14 +169,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
       TextView tv;
       if (view == null) {
         tv = new TextView(MainActivity.this);
-        tv.setPadding(4, 4, 4, 4);
+        int dp8 = (int) TypedValue.applyDimension(COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics());
+        tv.setPadding(dp8, dp8, dp8, dp8);
       } else {
         tv = (TextView) view;
       }
       FileScanner.FindItem item = mData.get(i);
-      mDate.setTime(item.modifyTime * 1000);
-      if (mNeedDetail) {
-        tv.setText(item.path.substring(item.path.lastIndexOf('/') + 1) + "   " + mFormat.format(mDate) + "   size:" + item.size / 1024 + "KB");
+      if (mCBDetail.isChecked()) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(item.path);
+        sb.append("\n");
+        mDate.setTime(item.modifyTime * 1000);
+        String md = mFormat.format(mDate);
+        sb.append(md);
+        sb.append("    ");
+        sb.append("size:");
+        sb.append(item.size / 1024);
+        sb.append("kb");
+        tv.setText(sb.toString());
       } else {
         tv.setText(item.path.substring(item.path.lastIndexOf('/') + 1));
       }
